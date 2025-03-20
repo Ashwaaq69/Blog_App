c5a8923b97a42bfa2730b0f056167e6bfc717b58
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2025-03-20 15:22:15 +0300

    updated the blog section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className='max-w-9/10 p-10 m-0 auto'>
-      <div className='flex justify-between items-center mb-20'>
+      <div className='flex justify-between items-center'>
         <a href="#"className="font-semibold text-2xl text-gray-600 ml-10">myBlog</a>
         <div className='flex justify-center items-center gap-4'> 
             <a href="#" className='text-lg text-gray-500'>login</a>
--- frontend/src/components/blog.jsx
+++ frontend/src/components/blog.jsx
@@ -8,14 +8,17 @@ import imagefour from '../assets/4.jpg'
 
 const blog = () => {
   return (
-    <div className="flex flex-col justify-between max-w-9/10 p-10 m-0 auto gap-8">
+    <div className="flex flex-col justify-between max-w-9/10 p-10 m-0 auto gap-8 m">
         
         <div className="flex justify-between gap-8">
             <img className="w-[300px] h-[260px] ml-8"  src={imageone} alt="Hero Image" />
             <div className='items-center'>
-                <h1 className='font-bold text-4xl mb-2'>How Chat GPT and Other Ai Tools Can Transform Our World</h1>
-                <small className="text-gray-400 mb-4">miki 2025-01-07 11:014</small>
-                <p className='text-2xl bold'>Have you heard about the new Ai tool called ChatGPT? it stands for Generate pre-trained Transformer, And its about you can talk to and have a conversation with.</p>
+                <h1 className='font-bold text-4xl mb-4'>How Chat GPT and Other Ai Tools Can Transform Our World</h1>
+                <p className="mb-2">
+                <a className="text-gray-400">miki </a>
+                <time className="text-gray-400">2025-01-07 11:014</time>
+                </p>
+                <p className='text-2xl bold mt-6'>Have you heard about the new Ai tool called ChatGPT? it stands for Generate pre-trained Transformer, And its about you can talk to and have a conversation with.</p>
             </div>
         </div>
 
