f3b0934696a87428e4b634483b11cc419394fabe
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2025-03-25 16:54:08 +0300

    created edit page api

--- frontend/src/pages/EditPage.jsx
+++ frontend/src/pages/EditPage.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
-
+import {useEffect, useState} from "react";
+import {Navigate, useParams} from "react-router-dom";
 const EditPage = () => {
+    const {id} = useParams();
+  const [title,setTitle] = useState('');
+  const [summary,setSummary] = useState('');
+  const [content,setContent] = useState('');
+  const [files, setFiles] = useState('');
+  const [redirect,setRedirect] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:5001/post/'+id)
+      .then(response => {
+        response.json().then(postInfo => {
+          setTitle(postInfo.title);
+          setContent(postInfo.content);
+          setSummary(postInfo.summary);
+        });
+      });
+  }, []);
     return (
         <div>
             <h1>Edit Page</h1>
