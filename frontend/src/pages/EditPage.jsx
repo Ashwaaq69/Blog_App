bc729e6025e1fd5303ac296d1b6315e02239256d
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2025-03-25 16:55:19 +0300

    created edit page api

--- frontend/src/pages/EditPage.jsx
+++ frontend/src/pages/EditPage.jsx
@@ -19,6 +19,29 @@ const EditPage = () => {
         });
       });
   }, []);
+  async function updatePost(ev) {
+    ev.preventDefault();
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('id', id);
+    if (files?.[0]) {
+      data.set('file', files?.[0]);
+    }
+    const response = await fetch('http://localhost:4000/post', {
+      method: 'PUT',
+      body: data,
+      credentials: 'include',
+    });
+    if (response.ok) {
+      setRedirect(true);
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={'/post/'+id} />
+  }
     return (
         <div>
             <h1>Edit Page</h1>
